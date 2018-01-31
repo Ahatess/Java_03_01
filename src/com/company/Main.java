868c7853521ca4c513826f60098f9920acc9b641
Andrius <andriuscere@gmail.com>
2018-01-31 10:29:11 +0200

    Uzduotis0301

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,16 +1,12 @@
 package com.company;
+import uzdaviniai.LyginisNelyginis;
+
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Iveskite skaiciu");
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        if(a%2 == 0){
-            System.out.println("Skaicius yra lyginis");
-        }else {
-            System.out.println("Skaicius yra nelyginis");
-        }
+        LyginisNelyginis objektas = new LyginisNelyginis();
+
     }
 }
---
+++ src/uzdaviniai/LyginisNelyginis.java
@@ -0,0 +1,15 @@
+package uzdaviniai;
+
+import java.util.Scanner;
+public class LyginisNelyginis {
+    public LyginisNelyginis(){
+        System.out.println("Iveskite skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%2 == 0){
+            System.out.println("Skaicius yra lyginis");
+        }else {
+            System.out.println("Skaicius yra nelyginis");
+    }
+    }
+}
