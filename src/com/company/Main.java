069421132d98826aae9635d14f6981c900d08357
Andrius <andriuscere@gmail.com>
2018-01-31 10:39:35 +0200

    Uzduotis0301

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Iveskite skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
 
         LyginisNelyginis objektas = new LyginisNelyginis();
+
+        objektas.setA(a);
+
+        System.out.println("Ivesta skaicius: " + objektas.getA());
+        objektas.skaiciuok();
 
     }
 }
